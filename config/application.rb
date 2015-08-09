d60308d6f1916b1e34610e1f62371c5a571299ae
jramoscr <jramos@pernix-solutions.com>
2015-08-09 11:48:43 -0600

    Prevent initialize of assets in the application

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Ecruzcontrol
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Prevent initializing your application and connecting to the database
+    config.assets.initialize_on_precompile = false
   end
 end
